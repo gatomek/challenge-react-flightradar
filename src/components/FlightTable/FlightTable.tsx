b9abd8cdab851585a0430bba45546e0651fa8754
Gatomek <tomasz.galinski@gmail.com>
2026-01-02 13:15:49 +0100

    refactoring

--- src/components/FlightTable/FlightTable.tsx
+++ src/components/FlightTable/FlightTable.tsx
@@ -3,23 +3,11 @@ import {MaterialReactTable, type MRT_ColumnDef, useMaterialReactTable,} from 'ma
 import {getFlightTableColumns} from "./getFlightTableColumns.ts";
 import type {Airplane} from "./model/Airplane.ts";
 import {getFlightData} from "./getTestFlightData.ts";
-import {useQuery} from "@tanstack/react-query";
-import type {AircraftData} from "../Query/model/AircraftData.ts";
+import {useAirplanesLiveApi} from "../hooks/useAirplanesLiveApi.ts";
 
 export function FlightTable() {
 
-    const {data} = useQuery({
-        queryKey: ['airplanes.21.51.100'],
-        queryFn: async (): Promise<AircraftData> => {
-            const res = await fetch('https://api.airplanes.live/v2/point/51.00/21.00/100');
-            if (!res.ok) {
-                throw new Error(`Failed to fetch flight data: ${res.status} ${res.statusText}`);
-            }
-            const json = await res.json();
-            return json as AircraftData;
-        }
-    });
-
+    const {data} = useAirplanesLiveApi();
     const flightData = useMemo<Airplane[]>(() => getFlightData(data), [data]);
     const columns = useMemo<MRT_ColumnDef<Airplane>[]>(
         () => getFlightTableColumns(), []
