3c286c0454473f7e42eecc2546b2480448bd6d72
Gatomek <tomasz.galinski@gmail.com>
2026-01-02 12:15:05 +0100

    review

--- src/components/Map/FlightMap.tsx
+++ src/components/Map/FlightMap.tsx
@@ -3,9 +3,8 @@ import type {LatLngTuple} from "leaflet";
 import type {CustomTileLayer} from "./CustomTileLayer.ts";
 import {stadiaMapsTileLayer} from "./tileLayers.ts";
 import {useState} from "react";
-import {SetViewCommand} from "./SetViewCommand.tsx";
 
-const DEFAULT_POSITION: [number, number] = [51.961301, 20.142209]
+const DEFAULT_POSITION: [number, number] = [20.142209, 51.961301]
 const position: LatLngTuple = [DEFAULT_POSITION[1], DEFAULT_POSITION[0]];
 
 function SetTileLayer(props: Readonly<CustomTileLayer>) {
@@ -20,7 +19,6 @@ function SetTileLayer(props: Readonly<CustomTileLayer>) {
 
 export function FlightMap() {
     const [tileLayer] = useState<CustomTileLayer>(stadiaMapsTileLayer);
-    const [marker] = useState<[number, number, number?]>(position);
 
     return (
         <MapContainer
@@ -30,7 +28,6 @@ export function FlightMap() {
             scrollWheelZoom={true}
         >
             <SetTileLayer url={tileLayer.url} attribution={tileLayer.attribution}/>
-            <SetViewCommand marker={marker}/>
         </MapContainer>
     )
 }
