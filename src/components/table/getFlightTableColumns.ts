86d744ecab70a470fc611ad2ced7365f93eb630e
Gatomek <37158592+gatomek@users.noreply.github.com>
2026-01-04 10:40:39 +0100

    adding mui virtual flight table (#10)

--- src/components/table/getFlightTableColumns.ts
+++ src/components/table/getFlightTableColumns.ts
@@ -4,39 +4,34 @@ import type {Airplane} from "./model/Airplane.ts";
 export function getFlightTableColumns(): MRT_ColumnDef<Airplane>[] {
     return [
         {
-            accessorKey: 'hex',
+            accessorFn: (row) => row.hex.toUpperCase(),
             header: 'ICAO',
-            size: 50,
+            size: 50
         },
         {
             accessorKey: 'flight',
             header: 'Flight',
-            size: 50,
+            size: 50
         },
         {
             accessorKey: 'registration',
             header: 'Register',
-            size: 50,
+            size: 50
         },
         {
             accessorKey: 'type',
             header: 'Type',
-            size: 50,
-        },
-        {
-            accessorKey: 'desc',
-            header: 'Desc',
-            size: 50,
+            size: 50
         },
         {
             accessorKey: 'altitude',
             header: 'Altitude',
-            size: 50,
+            size: 50
         },
         {
             accessorFn: (row: Airplane): "MIL" | undefined => row.military ? "MIL" : undefined,
             header: 'Info',
-            size: 30,
+            size: 50
         },
     ]
 }
