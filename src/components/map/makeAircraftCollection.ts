c0d565a2448f9d925ef7275fdf72b687c7ba8bda
Gatomek <tomasz.galinski@gmail.com>
2026-01-15 01:37:32 +0100

    hotfix

--- src/components/map/makeAircraftCollection.ts
+++ src/components/map/makeAircraftCollection.ts
@@ -31,7 +31,7 @@ export function makeAircraftCollection(data: AircraftData | undefined, icao: str
                 coordinates: [ac.lon, ac.lat]
             },
             properties: {
-                desc: "ICAO: " + ac.hex.toUpperCase() + '<br/>ALT: ' + ac.alt_baro + '<br/>TYPE: ' + ac.t + (ac.desc ? " | " + ac.desc : ''),
+                desc: "ICAO: " + hex + '<br/>ALT: ' + ac.alt_baro + '<br/>TYPE: ' + ac.t + (ac.desc ? " | " + ac.desc : ''),
                 type: ac.t,
                 icao: hex,
                 marker: icao !== '' && icao === hex,
